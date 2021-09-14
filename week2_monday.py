86e360d46a64930271be8ff8d55eda3b56336892
macroanton <mmorfin1001@gmail.com>
2021-09-13 23:37:12 -0500

    update
    update

---
+++ week2_monday.py
@@ -0,0 +1,11 @@
+
+#anything that begins with a # is a comment
+# comments are code that doesn't get executed
+
+print('hello world')
+print('goodbye')
+
+var1='hello'
+var = 'world'
+var1 = var1 + ' ' + var2
+print(var3)
